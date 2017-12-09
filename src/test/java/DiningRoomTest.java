cbb9ecda1ed9139fac51cd649aca724750335af8
derkrisz <krisztian.dergmail.com>
2017-12-09 14:38:25 +0000

    dining room has getters/tests

--- src/test/java/DiningRoomTest.java
+++ src/test/java/DiningRoomTest.java
@@ -11,5 +11,24 @@ public class DiningRoomTest {
     public void before(){
         diningroom = new DiningRoom(30, 2, "Dining room");
     }
-    
+
+    @Test
+    public void diningRoomhasName(){
+        assertEquals("Dining room", diningroom.getName());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(30, diningroom.getCapacity());
+    }
+
+    @Test
+    public void hasRoomNumber(){
+        assertEquals(2, diningroom.getNumber());
+    }
+
+    @Test
+    public void hasNoGuests(){
+        assertEquals(0, diningroom.getGuestsNumber());
+    }
 }
